bd0e34952ac4ae067093d46cdb9636f4031e3733
Josther-Malaya <josthermiguelmalaya@gmail.com>
2026-03-20 19:50:03 +0800

    second python skill

---
+++ printing_equal_number.py
@@ -0,0 +1,8 @@
+num1 = str(input("Enter first number: "))
+num2 = str(input("Enter second number: "))
+if num1 > num2:
+    print(num1 + " is bigger")
+elif num2 > num1:
+    print(num2 + " is bigger")
+elif num1 == num2:
+    print("both are equal numbers")
