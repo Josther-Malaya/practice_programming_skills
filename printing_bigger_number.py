cbc59cf2eeab0031a4fedc2b9912d682d71049ad
Josther-Malaya <josthermiguelmalaya@gmail.com>
2026-03-20 19:43:25 +0800

    initial commit

---
+++ printing_bigger_number.py
@@ -0,0 +1,6 @@
+num1 = str(input("Enter the first number: "))
+num2 = str(input("Enter the second number: "))
+if num1 > num2:
+    print(num1 + " is bigger")
+if num2 > num1:
+    print(num2 + " is bigger")
